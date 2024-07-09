e30c50861e625ae51795d32e48f032f4eaa0d7a8
Sweety <sweety@Harshitas-MBP.lan>
2024-07-08 23:49:20 -0700

    favicon and title

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import { Banner } from './components/Banner';
 import { Projects } from './components/Projects';
 import confetti from 'canvas-confetti';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8"/>
+        <title>nenu</title>
+        <link rel="canonical" href="https://nenuprelaunch.vercel.app"/>
+      </Helmet>
       <NavBar />
       <Banner />
       <Projects />

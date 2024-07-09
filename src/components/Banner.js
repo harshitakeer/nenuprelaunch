21d08a17aaff8aed96bae3d62783c1aedb2b3d5b
harshitakeer <115908428+harshitakeer@users.noreply.github.com>
2024-07-08 21:18:24 -0700

    Add files via upload

---
+++ src/components/Banner.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons"; 
+import headerImg from "../assets/img/yoyo.png";
+import confetti from 'canvas-confetti';
+import { Helmet } from 'react-helmet';
+
+export const Banner = () => {
+
+    useEffect(() => {
+        confetti({
+            particleCount: 100,
+            spread: 200,
+            origin: { y: 0.6 },
+            colors: ['#ff7fb5', '#46b8cd', '#3f7fa1', '#fef176', '#9f4c81'], // Optional: customize confetti colors
+            shapes: ['square', 'circle', 'star']    // Optional: customize confetti shapes
+        });
+    }, []);
+
+    return (
+        <>
+        <Helmet>
+            <title>nenu</title>
+            <link rel="icon" href="http://localhost:3000/title_logo.ico" type="image/png" />
+        </Helmet>
+      <section className="banner" id="home">
+        <Container>
+            <Row className="align-items-center">
+                <Col xs={12} md={6} xl={7}>
+                    <span className="tagline"> welcome to pre-launch of</span>
+                    <h1>{'nenu apparel'}<span className="wrap">{''}</span></h1>
+                    <p> create a customizable, fully embroidered mini-you from a variety of options given on the website. once you're satisfied, cop the merch and elevate your vibe. this can be u →</p>
+                </Col>
+                <Col xs={12} md={6} xl={5}>
+                    <img src={headerImg} alt="Header Img" className="header-img" />
+                </Col>
+            </Row>
+        </Container>
+      </section>  
+      </>
+    );
+}
+
---
+++ src/components/Projects.js
@@ -0,0 +1,76 @@
+import { Col, Container, Tab, Row, Nav } from "react-bootstrap";
+import { ProjectCard} from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/Nenu Projects (8).png";
+import projImg2 from "../assets/img/Nenu Projects (1).png";
+import projImg3 from "../assets/img/Nenu Projects (2).png";
+import projImg4 from "../assets/img/Nenu Projects (10).png";
+
+
+
+export const Projects = () => {
+    
+    const projects = [
+        {
+            title: "actions",
+            description: "to express yourself yo",
+            imgUrl: projImg1,
+        },
+        {
+            title: "hats",
+            description: "to complete the look",
+            imgUrl: projImg2,
+        },
+        {
+            title: "faces",
+            description: "to be silly!",
+            imgUrl: projImg3,
+        },
+        {
+            title: "hair",
+            description: "or no hair to be sleek",
+            imgUrl: projImg4,
+        },
+       
+    ];
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                    <h2>pieces</h2>
+                    <p>choose your own actions, faces, hats, and more for your own avatar! more pieces will be added and updated here. any suggestions or want to sign-up? scroll down and sign up using the google form!</p>
+                    <Tab.Container id="project-tabs" defaultActiveKey="first">
+                    <Nav variant = "pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                        </Nav>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="first">
+                                <Row>
+                                    {
+                                        projects.map((project,index) => {
+                                          return(
+                                            <ProjectCard
+                                              key={index}
+                                              {...project}
+                                              />
+                                          ) 
+                                        })
+                                    }
+                                </Row>
+                            </Tab.Pane>
+                        </Tab.Content>
+                        </Tab.Container>
+                        <div className="iframe-container">
+                            <iframe
+                                src="https://forms.gle/Ai2XoLc7EGUfZ8dx5"
+                                className="responsive-iframe"
+                                title="Project Feedback Form">Loading…</iframe>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2}></img>
+        </section>
+
+    )
+}
